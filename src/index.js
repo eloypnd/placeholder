1e42cd21d003ad1dee441f275882b9cf49a5eb67
Eloy Pineda <code@eloy.email>
2018-05-19 00:10:41 +0200

    refactor: add semantic-ui-css to app entry point

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
+import 'semantic-ui-css/semantic.min.css'
 import { Provider } from 'react-redux'
 import PlaceholderContainer from './containers/Placeholder'
 import store from './store'
